3ff8e7128e8fb5e19aa772d700e2c2acd62f477e
Gent <Gent@Windsor>
2016-10-27 23:36:42 -0400

    Added random messages
    If there is no message from twitter, after a set amount of time a random
    message will be sent.

--- Python/twitter_search.py
+++ Python/twitter_search.py
@@ -3,6 +3,7 @@ from TwitterSearch import *
 import re
 import serial
 from time import sleep
+import random
 #If you have a seperate credentials file, uncomment the next line, otherwise you can just edit the secret tokens below.
 #from credentials import *
 
@@ -10,41 +11,54 @@ regex = re.compile('[^a-zA-Z]')
 #Change this hashtag to what you want to search for
 sft = re.compile('#sparkfunthings')
 #change this to the port the Arduino is on
-myPort = serial.Serial('/dev/ttyUSB0', 115200, timeout = 10)
+myPort = serial.Serial('/dev/ttyACM0', 115200, timeout = 10)
 myPort.write("Hello, world!")
 
 tweet_id_list = []
+#the messages to be displayed if no tweets are found. You can put as many as you want.
+random_messages = ['Random Message One','Random Message Two']
+no_message = 0
 
 while True:
-	try:
-		print "Searching..."
-		tso = TwitterSearchOrder() # create a TwitterSearchOrder object
-		tso.set_keywords(['#sparkfunthings']) # let's define all words we would like to have a look for
-		tso.set_include_entities(False) # and don't give us all those entity information
+    if no_message < 1 : #how many times you want to wait before displaying a stored message
+        try:
+            print "Searching..."
+            tso = TwitterSearchOrder() #Create a Twitter search object
+            tso.set_keywords(['#sparkfunthings']) #define the hashtag to search for
+            tso.set_include_entities(False) #don't include entity info of the tweet
 
-	    # it's about time to create a TwitterSearch object with our secret tokens
-		ts = TwitterSearch(
-			consumer_key = 'twitter_consumer_key',
-			consumer_secret = 'twitter_consumer_secret',
-			access_token = 'twitter_access_token',
-			access_token_secret = 'twitter_access_token_secret'
-		     )
+            #create TwitterSearch object with API secret tokens
+            ts = TwitterSearch(
+                consumer_key = 'twitter_consumer_key',
+                consumer_secret = 'twitter_consumer_secret',
+                access_token = 'twitter_access_token',
+                access_token_secret = 'twitter_access_token_secret'
+                )
 
-	     # this is where the fun actually starts :)
-		for tweet in ts.search_tweets_iterable(tso):
-			if tweet['id'] not in tweet_id_list:
-				no_hashtag = sft.sub('',  tweet['text'])
-				text_only = regex.sub('', no_hashtag).encode('ascii', 'ignore')+'\r'
-				tweet_id_list.append(tweet['id'])
-                #Prints the entire tweet
-				print text_only
-                #Send the entire tweet over the serial port, instead of letter by letter
-				myPort.write(text_only)
-				sleep(5)
+            #start the actual search
+            for tweet in ts.search_tweets_iterable(tso):
+                if tweet['id'] not in tweet_id_list:
+                    no_hashtag = sft.sub('', tweet['text'])
+                    text_only = regex.sub('', no_hashtag).encode('ascii', 'ignore')+'\r'
+                    tweet_id_list.append(tweet['id'])
+                    #Prints the entire tweet
+                    print text_only
+                    #Send entire tweet over serial port
+                    myPort.write(text_only)
+                    sleep(5)
 
-	except TwitterSearchException as e: # take care of all those ugly errors if there are some
-	    print(e)
+        except TwitterSearchException as e: #Take care of errors
+            print(e)
 
-	sleep(60)
+        no_message +=1
+        print "No message counter:" + no_message
+
+    else:
+        random_message = random.choice(random_messages)
+        print random_message
+        myPort.write(random_message)+'\r'
+        no_message = 0
+        print "No message counter:" + no_message
+    sleep(60)
 
 myPort.close()
